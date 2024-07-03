387821a65c72f01fdce8471861cb612bb6fb7183
CodeMaverick2 <codemaverick2@gmail.com>
2024-07-03 10:34:05 +0530

    Put call updated

--- express/index.js
+++ express/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 let courses = [
     {id:1, name: "java"},
@@ -10,7 +11,15 @@ app.get('/courses',(req,res)=>{
     res.json(courses);
 });
 
+app.post('/courses', (req, res) => {
+    console.log(req.body)
+    let newCourse = { id: courses.length+1, name: req.body.name };
+    courses.push(newCourse);
+    res.send(courses);
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
