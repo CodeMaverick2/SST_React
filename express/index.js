b881a308a793fc5cc655bf6d96a1a6ef31e7b2e7
CodeMaverick2 <codemaverick2@gmail.com>
2024-07-03 10:50:01 +0530

    Put Delete Functionality

--- express/index.js
+++ express/index.js
@@ -18,8 +18,34 @@ app.post('/courses', (req, res) => {
     res.send(courses);
 });
 
+app.put('/courses', (req, res) => {
+    const courseId = parseInt(req.body.id);
+    const updatedData = req.body;
+
+    const course = courses.find(c => c.id === courseId);
+    course.name = updatedData.name || course.name;
+
+    res.json({ data: course });
+});
+
+app.delete('/courses', (req, res) => {
+    const courseId = parseInt(req.body.id);
+
+    const courseIndex = courses.findIndex(c => c.id === courseId);
+
+    courses.splice(courseIndex, 1);
+
+    res.json({ data: courses });
+});
 const PORT = 3000;
-app.listen(PORT, () => {
+app.listen(PORT, () => {app.put('/courses', (req, res) => {
+    courses = courses.map(course => {
+        course.id += 5;
+        return course;
+    });
+    res.json(courses);
+});
+
     console.log(`Server is running on http://localhost:${PORT}`);
 });
 
